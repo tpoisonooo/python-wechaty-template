88b5867eba1a866ca665bf45a902c37e0a216a63
tpoisonooo <khj.application@aliyun.com>
2024-01-31 13:25:38 +0800

    Update ding_dong.py

--- src/plugins/ding_dong.py
+++ src/plugins/ding_dong.py
@@ -2,16 +2,33 @@ from wechaty import WechatyPlugin, Message
 from quart import Quart, render_template_string
 from wechaty_plugin_contrib.message_controller import message_controller
 from wechaty import WechatyPlugin
+import requests
+import json
 
 class DingDongPlugin(WechatyPlugin):
     VIEW_URL = '/api/plugins/ding_dong/view'
 
     @message_controller.may_disable_message
     async def on_message(self, msg: Message) -> None:
-        if msg.text() == "ding":
+        text = msg.text()
+        if text == "ding":
             await msg.say("dong")
-            await msg.say("I'm alive ...")
-            message_controller.disable_all_plugins(msg)
+            
+        # use your own IP !!!
+        url = 'http://127.0.0.1:9527/api'
+        data = {
+            'query': text
+        }
+        data_json = json.dumps(data)
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        # 发起 POST 请求
+        response = requests.post(url, data=data_json, headers=headers, timeout=200)
+        await msg.say(response.text)
+
+        message_controller.disable_all_plugins(msg)
 
     async def blueprint(self, app: Quart) -> None:
         
